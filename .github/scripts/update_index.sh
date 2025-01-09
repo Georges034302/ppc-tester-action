13a6980f940deb93a68afcf4686690068c1ddc02
Georges034302 <georges.boughantous@gmail.com>
2025-01-09 13:30:54 +1100

    ci - update index

--- .github/scripts/update_index.sh
+++ .github/scripts/update_index.sh
@@ -6,7 +6,6 @@ GITHUB_USER=$2
 TIMESTAMP=$(date)
 
 # Update README
-echo -e "\n[$GITHUB_USER - $FREQ_RESULT - $TIMESTAMP]" >> README.md
 sed -i '/<\/body>/i <p>Updated by Team Leader: '"[$GITHUB_USER - $FREQ_RESULT - $TIMESTAMP]"'</p>' index.html
 
 # Configure Git to use GitHub Actions user and email
@@ -14,6 +13,6 @@ git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
 
 # Add README.md to git, commit, and push the changes
-git add README.md
+git add index.html
 git commit -m "Update index.html with vowel frequency results"
 git push
